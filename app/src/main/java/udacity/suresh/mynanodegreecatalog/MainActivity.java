1872ef24c96997d260baa3e026f991933c73f74b
Sureshkumar <sureshkumarb22792@gmail.com>
2015-11-20 15:49:49 +0530

    added scrollview to support landscape orientation, made toast message responsive on button click.

--- app/src/main/java/udacity/suresh/mynanodegreecatalog/MainActivity.java
+++ app/src/main/java/udacity/suresh/mynanodegreecatalog/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    private Toast mToast;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,29 +18,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-        String message = "";
-        switch(view.getId()){
-            case R.id.btn_spotify:
-                message = "This button will launch Spotify streamer";
-                break;
-            case R.id.btn_scores:
-                message = "This button will launch Scores Application";
-                break;
-            case R.id.btn_library:
-                message = "This button will launch My Library Application";
-                break;
-            case R.id.btn_bigger:
-                message = "This button will launch an Application that will be built something bigger!";
-                break;
-            case R.id.btn_xyz_reader:
-                message = "This button will launch a reader";
-                break;
-            case R.id.btn_my_own_app:
-                message = "This button will launch My own application";
-                break;
-            default:
-                return;
+        if(mToast != null){
+            mToast.cancel();
         }
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        mToast = Toast.makeText(this, ((Button)view).getText().toString(), Toast.LENGTH_SHORT);
+        mToast.show();
     }
 }
